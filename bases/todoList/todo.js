7c7d73a7546ff822f1e236575390c802b7b6f485
Julian Sucerquia <andresucerquia@gmail.com>
2024-04-17 18:27:50 -0500

    feat: :construction: Create todo list logic

--- bases/todoList/todo.js
+++ bases/todoList/todo.js
@@ -1,10 +1,9 @@
-let todoList = []
-
 // Agregar a la lista
+let todoList = []
 
 function addToList(item) {
   let todoListItem = {
-    id: todoList.length + 1,
+    id: new Date().getTime(),
     title: item,
     isCheck: false,
   }
@@ -12,24 +11,72 @@ function addToList(item) {
   todoList.push(todoListItem)
 }
 
-function showTasks() {
-  console.log("-".repeat(20))
-  for (item of todoList) {
-    let isCompleted = item.isCheck ? "Completed" : "Not completed"
-    // console.log(item.id + ": " + item.title + " [" + isCompleted + " ]")
-    console.log(`${item.id}: ${item.title} [${isCompleted}]`)
+function showTasks(list) {
+  try {
+    console.log("-".repeat(20))
+    for (item of list) {
+      let isCompleted = item.isCheck ? "Completed" : "Not completed"
+      // console.log(item.id + ": " + item.title + " [" + isCompleted + " ]")
+      console.log(`${item.id}: ${item.title} [${isCompleted}]`)
+    }
+    console.log("-".repeat(20))
+  } catch (error) {
+    console.log("ocurrió un error agregando la tarea")
   }
-  console.log("-".repeat(20))
+}
+
+// Buscar tarea
+function findTask(taskId) {
+  let id = todoList.findIndex((item) => item.id === taskId)
+  if (id >= 0) {
+    return id
+  } else {
+    console.error("No existe el elemento en la lista")
+  }
 }
 // Eliminar de una lista
-// marcar como completada una tarea
-function completeTask(taskId) {}
+function deleteTask(taskId) {
+  try {
+    let id = findTask(taskId)
+    if (id !== undefined) {
+      todoList.splice(id, 1)
+      console.log("Se eliminó correctamente la tarea con id: " + taskId)
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function persistTodoList() {
+  localStorage.setItem("todoList", JSON.stringify(todoList))
+}
+
+// Marcar como completada una tarea
+function completeTask(taskId) {
+  try {
+    // let data = todoList.find((item) => item.id === taskId)
+    // data.isCheck = true
+    let id = findTask(taskId)
+
+    if (id !== undefined) {
+      todoList[id].isCheck = true
+      console.log("Se actualizó el estado de la tarea con id: " + taskId)
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
 // listar las tareas pendientes
+function showPendingTasks() {
+  let filteredList = todoList.filter((item) => !item.isCheck)
+  showTasks(filteredList)
+}
 // ver toda mi lista de tareas
 
 function main() {
   let option
   do {
+    let id
     option = prompt(
       "Por favor ingrese una opcion: \n 1. Agregar\n 2. Eliminar\n 3. Marcar como completado\n 4. Ver tareas pendientes\n 5. ver todas las tareas \n 0. Para terminar el programa"
     )
@@ -37,24 +84,27 @@ function main() {
       case "1":
         let task = prompt("Ingrese el nombre de la tarea")
         addToList(task)
-        showTasks()
+        persistTodoList()
+        showTasks(todoList)
         break
       case "2":
-        console.log("eliminar de la lista")
+        id = prompt("Ingrese el ID de la tarea que quieres eliminar")
+        deleteTask(parseInt(id))
+        persistTodoList()
         break
       case "3":
-        console.log("marcar como completado")
-        let id = prompt("Ingrese el ID de la tarea que quieres completar")
-        completeTask(id)
+        id = prompt("Ingrese el ID de la tarea que quieres completar")
+        completeTask(parseInt(id))
+        persistTodoList()
         break
       case "4":
-        console.log("ver las tareas pendientes")
+        showPendingTasks()
         break
       case "5":
-        showTasks()
+        showTasks(todoList)
         break
       default:
-        console.log("Opcion incorrecta")
+        console.log("Opción incorrecta")
         break
     }
   } while (option !== "0")
